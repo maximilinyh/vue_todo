06dceaa6d8e11466727b04431faf40049eab144d
Maxim Ilinyh <maxim_ilinyh@mail.ru>
2020-05-10 23:22:47 +0700

    commit 10/05

---
+++ src/components/List.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="test">
+        <h1>Тестовый компонент</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Test'
+    }
+</script>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="items-toolbar">
+        <button class="items-toolbar__btn">Completed</button>
+        <button class="items-toolbar__btn">Need to do</button>
+        <button class="items-toolbar__btn">Remove all</button>
+    </div>
+</template>
